1297ecbabe26f2e5626f68458fd83453261d0ce6
Rachel Peterson <rachelp5446@gmail.com>
2020-10-22 18:07:23 -0500

    added Sass, styled login

--- readerly/settings.py
+++ readerly/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sass_processor',
     'eventsApp',
     'readerly_app',
     'django.contrib.admin',
@@ -120,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+
+
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static')
